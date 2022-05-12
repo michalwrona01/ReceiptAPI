e749d8c83c95fee4f8e4bb746e5cf9a4806661f0
Michał Wrona <wronamichal01@gmail.com>
2022-05-12 20:59:28 +0200

    Added Product and Receipt models

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, Date, DateTime, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from database import Base
 
 
@@ -10,3 +11,30 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+
+    receipts = relationship("Receipt")
+
+
+class Receipt(Base):
+    __tablename__ = "receipt"
+
+    id = Column(Integer, primary_key=True, index=True)
+    shop_name = Column(String)
+    address = Column(String)
+    date_add_receipt = Column(Date, default=func.now())
+    date_shop_products = Column(DateTime(timezone=True), default=func.now())
+    number_receipt = Column(Integer)
+    NIP_number_shop = Column(Integer)
+    products = relationship("Product")
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    price = Column(Float)
+    amount = Column(Float)
+
+    receipt_id = Column(Integer, ForeignKey("receipt.id"))
